40a5c08f65f24abcae677b416dc618bb5795592e
JordiAlcalde <jordi.alcalde@outlook.com>
2025-11-21 17:25:36 +0100

    Add files via upload

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,7 +1,8 @@
-/// <reference types="vite/client" />
+declare module "*.css";
 
 declare namespace NodeJS {
   interface ProcessEnv {
     API_KEY: string;
+    [key: string]: string | undefined;
   }
 }
